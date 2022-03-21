29d875fb552b875eb975b287460cb3e7c5b1c4d9
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-03-22 00:44:36 +0900

    Feat : Cors 모듈 설치, 옵션 추가

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import logger from 'morgan';
 import router from './routes/index.js';
+import cors from 'cors';
+
+import { corsOption } from './Common/Constant/index.js';
 
 const app = express();
 
+app.use(cors(corsOption));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
