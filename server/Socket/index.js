8eee140e4bb7fb0b0ae2ebef15f8c22de8af9cde
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-03-28 15:47:45 +0900

    Feat : 소켓 이벤트 추가

--- server/Socket/index.js
+++ server/Socket/index.js
@@ -1,10 +1,37 @@
 import { Server } from 'socket.io';
 import { corsOption } from '../Common/Constant/index.js';
-import { printConnection } from './util.js';
+import { printConnection, joinRoom, chat, keyUp, keyDown } from './util.js';
+
 export const socketInit = (server, app) => {
     console.log(corsOption);
     const io = new Server(server, { cors: corsOption });
     io.on('connection', (socket) => {
         printConnection(socket);
+        socket.on('join', joinRoom);
+        socket.on('chat', chat);
+        socket.on('keyUp', keyUp);
+        socket.on('keyDown', keyDown);
     });
 };
+
+/*
+ socket 이벤트 
+----  
+eventName : "join" 
+Input : (roomId,userName or userId)
+Output : { characterId {number}, Position {x,y,z} }
+----
+eventName : "keyDown" (애니메이션 포함?) => broadcast
+Input : userName or userId, key, Position 
+Output : useName or userId, key, Position 
+---- 
+eventName : "keyUp" (애니메이션 포함?) => broadcast 
+Input : userName or userId, key, Position 
+Output : useName or userId, key, Position 
+
+---
+eventName : "chat"
+Input : userName or userId, message , Position
+Output : userName or userId, message, Position
+----
+*/
--- server/Socket/util.js
+++ server/Socket/util.js
@@ -1,6 +1,38 @@
+export const UserIdToRoom = new Map();
+
+export const getJoinRoom = (userId) => UserIdToRoom.get(userId);
+
 export const printConnection = (socket) => {
     const req = socket.request;
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log('socket 연결 성공 ip : ', ip);
     console.log('socket id : ', socket.id);
+};
+
+export const joinRoom = ({ roomId, userId }) => {
+    UserIdToRoom.set(userId, roomId);
+    this.join(roomId);
+    this.to(roomId).emit('join', {
+        characterId: 1,
+        position: { x: 1, y: 7, z: 1 },
+    });
+};
+
+export const chat = ({ userId, message, position }) => {
+    const roomId = getJoinRoom(userId);
+    this.to(roomId).emit('chat', {
+        userId,
+        message,
+        position,
+    });
+};
+
+export const keyUp = ({ userId, key, position }) => {
+    const roomId = getJoinRoom(userId);
+    this.to(roomId).emit('keyUp', { userId, key, position });
+};
+
+export const keyDown = ({ userId, key, position }) => {
+    const roomId = getJoinRoom(userId);
+    this.to(roomId).emit('keyDown', { userId, key, position });
 };
