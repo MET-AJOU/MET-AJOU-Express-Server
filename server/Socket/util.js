d42f741232e140995f3fa002e78d4510992ff8de
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-04-08 21:42:49 +0900

    Fix : getUserState 함수 수정

--- server/Socket/util.js
+++ server/Socket/util.js
@@ -1,7 +1,11 @@
-export const getJoinedUsersState = (UsersState, UserIdToRoom, roomId) => Array.from(UsersState.entries()).map(([userId, userState]) => {
-  if (roomId === UserIdToRoom.get(userId)) return userState;
-});
+export const getJoinedUsersState = (UsersState, UserIdToRoom, roomId) =>
+    Array.from(UsersState.entries()).map(([userId, userState]) => {
+        if (roomId === UserIdToRoom.get(userId)) return userState;
+    });
 
-export const getUserState = (UsersState, _userId) => Array.from(UsersState.entries()).map(([userId, userState]) => {
-  if (userId === _userId) return userState;
-})[0];
+export const getUserState = (UsersState, _userId) => {
+    return [...UsersState.values()].filter((userState) => {
+        const { userId } = userState;
+        if (userId === _userId) return userState;
+    })[0];
+};
