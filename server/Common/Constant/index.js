f3bf0609ca4378b8c152081f56ce405bab92201a
jin-Pro <dnjun2@ajou.ac.kr>
2022-04-08 21:49:37 +0900

    Fix

--- server/Common/Constant/index.js
+++ server/Common/Constant/index.js
@@ -1,11 +1,6 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 export const corsOption = {
-<<<<<<< HEAD
-    // origin: `${process.env.WEB_SERVER_URL}`,
-    origin: `${process.env.WEB_SERVER_URL}:${process.env.WEB_SERVER_PORT}`,
-=======
   // origin: `${process.env.WEB_SERVER_URL}`,
   origin: `${process.env.WEB_SERVER_URL}:${process.env.WEB_SERVER_PORT}`,
->>>>>>> upstream/main
 };
