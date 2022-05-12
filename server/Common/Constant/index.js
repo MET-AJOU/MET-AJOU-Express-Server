fadeb41c7dc0fca1fe59b646a3e466737f881642
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-05-13 02:21:09 +0900

    Fix: package.json

--- server/Common/Constant/index.js
+++ server/Common/Constant/index.js
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 export const corsOption = {
-  // origin: `${process.env.WEB_SERVER_URL}`,
-  origin: `${process.env.WEB_SERVER_URL}:${process.env.WEB_SERVER_PORT}`,
+    // origin: `${process.env.WEB_SERVER_URL}`,
+    // origin: `${process.env.WEB_SERVER_URL}:${process.env.WEB_SERVER_PORT}`,
+    origin: `https://www.metajou.kro.kr`,
 };
