4da4ad085394df2bce3d553a70929e4933eb40b1
jin-Pro <dnjun2@ajou.ac.kr>
2022-05-30 21:23:20 +0900

    fix: init position debate Map

--- server/Socket/constant.js
+++ server/Socket/constant.js
@@ -1,27 +1,27 @@
 export const defaultKeyBoardState = {
-    backward: false,
-    forward: false,
-    left: false,
-    right: false,
-    boost: false,
-    space: false,
-    dance: false,
+  backward: false,
+  forward: false,
+  left: false,
+  right: false,
+  boost: false,
+  space: false,
+  dance: false,
 };
 
 export const DefaultPosition = {
-    1: {
-        x: -11.4,
-        y: 3,
-        z: 9.5
-    },
-    2: {
-        x: 0,
-        y: 0,
-        z: 0
-    },
-    3: {
-        x: 0,
-        y: 3,
-        z: 0
-    },
+  1: {
+    x: -11.4,
+    y: 3,
+    z: 9.5,
+  },
+  2: {
+    x: 0,
+    y: 0,
+    z: 0,
+  },
+  3: {
+    x: 0,
+    y: 0,
+    z: 0,
+  },
 };
