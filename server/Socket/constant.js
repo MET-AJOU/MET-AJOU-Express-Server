1ed40bccd087ab6300c5b678ff5426e2cba6e4bb
jin-Pro <dnjun2@ajou.ac.kr>
2022-06-01 16:19:17 +0900

    merge : constant

--- server/Socket/constant.js
+++ server/Socket/constant.js
@@ -1,31 +1,31 @@
 export const defaultKeyBoardState = {
-    backward: false,
-    forward: false,
-    left: false,
-    right: false,
-    boost: false,
-    space: false,
-    hello:false,
-    dance: false,
-    happy:false,
-    question:false,
-    lose:false
+  backward: false,
+  forward: false,
+  left: false,
+  right: false,
+  boost: false,
+  space: false,
+  hello: false,
+  dance: false,
+  happy: false,
+  question: false,
+  lose: false,
 };
 
 export const DefaultPosition = {
-    1: {
-        x: -11.4,
-        y: 3,
-        z: 9.5
-    },
-    2: {
-        x: 0,
-        y: 0,
-        z: 0
-    },
-    3: {
-        x: 0,
-        y: 3,
-        z: 0
-    },
+  1: {
+    x: -11.4,
+    y: 3,
+    z: 9.5,
+  },
+  2: {
+    x: 0,
+    y: 0,
+    z: 0,
+  },
+  3: {
+    x: 0,
+    y: 0,
+    z: 0,
+  },
 };
